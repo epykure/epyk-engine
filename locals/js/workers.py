632fd4d2a7d53412298938a579bb84a50f8d67e0
marlyk <smith.pyotr@gmail.com>
2021-03-11 08:21:36 +0000

    Update workers.py

--- locals/js/workers.py
+++ locals/js/workers.py
@@ -7,7 +7,6 @@ page = Report()
 page.headers.dev()
 
 w = page.js.worker()
-
 w.connect(content='''
 self.addEventListener('message', function(e) {
   var data = e.data;
@@ -16,8 +15,7 @@ self.addEventListener('message', function(e) {
       self.postMessage('WORKER STARTED TEST: ' + data.msg);
       break;
     case 'stop':
-      self.postMessage('WORKER STOPPED: ' + data.msg +
-                       '. (buttons will no longer work)');
+      self.postMessage('WORKER STOPPED: ' + data.msg + '. (buttons will no longer work)');
       self.close(); // Terminates the worker.
       break;
     default:
@@ -27,10 +25,7 @@ self.addEventListener('message', function(e) {
 ''')
 
 div = page.ui.div()
-
 page.ui.button("Say HI").click([w.postMessage({'cmd': 'start', 'msg': 'Hi'})])
 page.ui.button("Send unknown command").click([w.postMessage({'cmd': 'test', 'msg': 'test'})])
 page.ui.button("Stop worker").click([w.postMessage({'cmd': 'stop', 'msg': 'Bye'})])
-
-
 w.on('message', [div.build(w.message)])
