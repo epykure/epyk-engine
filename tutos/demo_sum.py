5c8f21cf1dcf361cab764f8f252989522d8ee306
marlyk <smith.pyotr@gmail.com>
2021-04-12 22:06:42 +0100

    Update demo_sum.py

--- tutos/demo_sum.py
+++ tutos/demo_sum.py
@@ -5,13 +5,13 @@ page = Report()
 template = page.body.add_template(defined_style="doc")
 template.style.css.background = "white"
 
-value1 = page.ui.fields.input(label="Value 1")
-value2 = page.ui.fields.input(label="Value 2")
-result = page.ui.text("&nbsp;", tooltip="Result")
+value1 = page.ui.fields.number(label="Value 1", options={"scroll": False})
+value2 = page.ui.fields.number(label="Value 2")
+result = page.ui.fields.text("&nbsp;", label="Result", tooltip="Result")
 result.style.css.border = "1px solid red"
 result.style.css.padding = 5
 result.style.css.min_width = 50
 
 page.ui.button("Sum", align="center").click([
-  result.build(value1.dom.content.number + value2.dom.content.number)
+  result.input.build(value1.dom.content.number + value2.dom.content.number)
 ])
