1d77704be130a19f79c19bae15ee066f9b3c2bcd
marlyk <smith.pyotr@gmail.com>
2021-04-13 22:15:12 +0100

    Update fintech_analytics.py

--- websites/ressources/fintech_analytics.py
+++ websites/ressources/fintech_analytics.py
@@ -12,9 +12,9 @@ TEXT_OPENSOURCE = "Open Source framework / No cost"
 
 CONTENT = [
   {"title": 'Fast prototyping', 'icon': 'far fa-lightbulb',
-   'content': 'Innovate and prototype easily by only using one page. Epyk will link to the underlying technologies for you.'},
-  {"title": 'Ease of use', 'icon': 'fas fa-user-friends',
-   'content': 'Anyone can use and an produce amazing and highly configurable web pages. At the end you only import the packages you really need'},
+   'content': 'Innovate and prototype easily by only using one page. Epyk will make the link to the underlying technologies for you.'},
+  {"title": 'Ease & Efficient', 'icon': 'fas fa-user-friends',
+   'content': 'Anyone can use and produce amazing and highly configurable web pages. At the end you only import the packages you really need.'},
   {"title": 'web dashboards', 'icon': 'fas fa-tv',
    'content': 'Combine multiple views and scripts of data to get richer insight. Share your results and advertise your findings.'},
   {"title": 'Big data visualisation', 'icon': 'fas fa-globe-europe',
@@ -29,26 +29,26 @@ LIBRARY_SPEC = "+ View API Documentation"
 
 VIGNETS = [
   {"image": "https://raw.githubusercontent.com/epykure/epyk-studio/master/static/shops.PNG", 'title': 'FROM E-COMMERCE',
-   "content": '''The library will provide a rich range of component ready to use to design your web page. 
-Look and feel of the page and each component can be changed. Nearly 100 external packages are currently available to give you
-a full control of the final result you can share with your friends or team.
+   "content": '''The library will provide a rich range of components ready to use to design your web pages. 
+Look and feel of the pages and each component can be changed. Nearly 100 external packages are currently available to give you
+a full control of the final results you can share with your friends or colleagues.
 '''},
   {"image": "https://github.com/epykure/epyk-templates/blob/master/static/images/charts.PNG?raw=true", 'title': 'TO DATA SCIENCE',
    "content": '''
-Epyk can also be used to display results and explore data. Any script dedicated to call an API or analyse data can be interface with Epyk.
+Epyk can also be used to display results and explore data. Any script dedicated to call an API or to analyse data can be interfaced with Epyk.
 This could then allow to share your findings, advertise your results and create your community!
 '''},
 ]
 
 QUOTE = '''
-Using Epyk is quite easy and it allow to write JavaScript, HTML and CSS without any knowledge on those domains.
-It is really a toolbox to help Python developers to learn the web, share their APIs and get this interfaced quickly to the application.
+Using Epyk is quite easy to use and it allows to write JavaScript, HTML and CSS without any knowledge in those domains.
+It is really a toolbox to help Python developers to learn the web, share their ideas and get them exposed quickly to clients.
 '''
 
 BANNER_ABOUT = {
   'title': "About Epyk",
   'content': '''
-Epyk target is to allow light and advanced Python developer to give a new dimension to their scripts.
+Epyk target is to allow light and advanced Python developers to give a new dimension to their scripts.
 Thanks to this library, they will be able to generate rich dashboard or web pages to share and integrate to the most popular web frameworks.
 From global enterprises to early-stage startups and small businesses, people everywhere use Epyk to improve time to market and team collaboration.
 '''
