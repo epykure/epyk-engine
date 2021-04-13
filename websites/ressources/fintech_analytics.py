f3a99666594a3fe81db4ca6106725ddb53d608e5
marlyk <smith.pyotr@gmail.com>
2021-04-13 22:04:14 +0100

    Fintech analytic template

---
+++ websites/ressources/fintech_analytics.py
@@ -0,0 +1,54 @@
+BACKGROUND_IMAGE = "https://github.com/epykure/epyk-templates/blob/master/static/images/banner_screen.PNG?raw=true"
+SLOGAN = "We're changing the way you think about data"
+
+BUTTON_TRY = "Add a star to the repo"
+BUTTON_TWITTER = "Follow us on twitter"
+BUTTON_SEE = "Learn from templates"
+
+
+BANNER_TEXT = "Epyk is a full stack and Open Source Web library."
+TEXT_OPENSOURCE = "Open Source framework / No cost"
+
+CONTENT = [
+  {"title": 'Fast prototyping', 'icon': 'far fa-lightbulb',
+   'content': 'Innovate and prototype easily by only using one page. Epyk will link to the underlying technologies for you.'},
+  {"title": 'Ease of use', 'icon': 'fas fa-user-friends',
+   'content': 'Anyone can use and an produce amazing and highly configurable web pages. At the end you only import the packages you really need'},
+  {"title": 'web dashboards', 'icon': 'fas fa-tv',
+   'content': 'Combine multiple views and scripts of data to get richer insight. Share your results and advertise your findings.'},
+  {"title": 'Big data visualisation', 'icon': 'fas fa-globe-europe',
+   'content': 'Use Big data module from Python or AWS services to interface your results.'},
+  {"title": 'Reliable & Configurable', 'icon': 'fas fa-wrench',
+   'content': 'Generate either on the fly or in a static manner web dashboard which can integrated to JavaScript ecosystems and fully tested.'},
+  {"title": 'Share in seconds', 'icon': 'fas fa-comments',
+   'content': 'Publish a dashboard with a few clicks to share it live on the web and on mobile devices.'},
+]
+
+LIBRARY_SPEC = "+ View API Documentation"
+
+VIGNETS = [
+  {"image": "https://raw.githubusercontent.com/epykure/epyk-studio/master/static/shops.PNG", 'title': 'FROM E-COMMERCE',
+   "content": '''The library will provide a rich range of component ready to use to design your web page. 
+Look and feel of the page and each component can be changed. Nearly 100 external packages are currently available to give you
+a full control of the final result you can share with your friends or team.
+'''},
+  {"image": "https://github.com/epykure/epyk-templates/blob/master/static/images/charts.PNG?raw=true", 'title': 'TO DATA SCIENCE',
+   "content": '''
+Epyk can also be used to display results and explore data. Any script dedicated to call an API or analyse data can be interface with Epyk.
+This could then allow to share your findings, advertise your results and create your community!
+'''},
+]
+
+QUOTE = '''
+Using Epyk is quite easy and it allow to write JavaScript, HTML and CSS without any knowledge on those domains.
+It is really a toolbox to help Python developers to learn the web, share their APIs and get this interfaced quickly to the application.
+'''
+
+BANNER_ABOUT = {
+  'title': "About Epyk",
+  'content': '''
+Epyk target is to allow light and advanced Python developer to give a new dimension to their scripts.
+Thanks to this library, they will be able to generate rich dashboard or web pages to share and integrate to the most popular web frameworks.
+From global enterprises to early-stage startups and small businesses, people everywhere use Epyk to improve time to market and team collaboration.
+'''
+}
---
+++ websites/template_fintech_home.py
@@ -0,0 +1,97 @@
+import epyk as pk
+from ressources import fintech_analytics
+
+
+page = pk.Page()
+
+qrcode = page.ui.qrcode("https://www.linkedin.com/notifications/")
+qrcode.style.css.fixed(bottom=10, left=10)
+qrcode.style.css.cursor = "pointer"
+qrcode.style.css.z_index = 300
+
+
+page.ui.contents().style.css.hide()
+
+button_css = {"border-radius": '10px', "border": '1px solid %s' % page.theme.colors[-1], 'color': page.theme.colors[-1],
+              'margin-bottom': '20px'}
+
+logo = page.ui.div(height=(30, "px"), width=(30, "px"))
+logo.style.css.background_url(
+  "https://raw.githubusercontent.com/epykure/epyk-ui/master/epyk/static/images/epyklogo.ico", size="auto 30px")
+logo.style.css.display = "inline-block"
+logo.style.css.margin_right = "10px"
+
+company = page.ui.text("Epyk")
+company.style.css.font_size = "17px"
+company.style.css.font_weight = 800
+company.style.css.text_transform = 'uppercase'
+company.style.css.letter_spacing = '2px'
+
+nav = page.ui.navbar(logo=page.ui.div([logo, company]), height=(80, "px"))
+nav.style.css.padding_left = "30px"
+nav.style.css.position = "None"
+
+bg = page.ui.images.background(fintech_analytics.BACKGROUND_IMAGE, height=(360, "px"), position="middle")
+bg.style.css.color = page.theme.colors[-1]
+bg.style.css.bold()
+
+slogan = page.ui.text(fintech_analytics.SLOGAN, align="center", width=(80, "%"))
+slogan.style.css.font_size = "60px"
+slogan.style.css.display = "inline-block"
+slogan.style.css.margin_top = 90
+bg.add(slogan)
+
+try_epyk = page.ui.buttons.large(fintech_analytics.BUTTON_TRY)
+try_epyk.css(button_css).css(
+  {'color': 'white', 'background': page.theme.notch(2), 'font-size': page.body.style.globals.font.normal(8), 'margin-right': '20px'})
+
+see_epyk = page.ui.buttons.large(fintech_analytics.BUTTON_SEE)
+see_epyk.no_background()
+see_epyk.css(button_css).css(
+  {'background': "white", 'color': page.theme.notch(2), "border": '1px solid %s' % page.theme.notch(2),
+   'font-size': page.body.style.globals.font.normal(8)})
+
+bg.add(page.ui.div([try_epyk, see_epyk]).css({"text-align": 'center'}))
+
+banner = page.ui.banners.text(fintech_analytics.BANNER_TEXT, size_notch=25, align="center")
+banner.style.css.width = "80%"
+
+rows, row = [], []
+for ct in fintech_analytics.CONTENT:
+  row.append(page.ui.vignets.vignet(title=ct["title"], icon=ct["icon"], content=ct["content"], render="row", width=(100, "%")))
+  if len(row) == 2:
+    rows.append(row)
+    row = []
+if row:
+  rows.append(row)
+
+grid = page.ui.grid(rows, width=(80, "%"))
+grid.style.css.margin_bottom = 40
+
+try_button = page.ui.button(fintech_analytics.BUTTON_TWITTER.upper(), icon="fab fa-twitter", align="center")
+try_button.css(button_css)
+try_button.no_background()
+
+link = page.ui.link(fintech_analytics.LIBRARY_SPEC, url="#specTech", html_code="specTech")
+link.style.css.color = "orange"
+
+specs = page.ui.div("test")
+specs.style.css.display = False
+
+link.click([specs.dom.toggle()])
+col = page.ui.col([try_button, page.ui.text(fintech_analytics.TEXT_OPENSOURCE, align="center"), link, specs])
+
+banner = page.ui.banners.text(col, background="#fafafa")
+
+row = [page.ui.vignets.image(title=v["title"], render="col", image=v["image"], content=v["content"]) for v in fintech_analytics.VIGNETS]
+row_vignets = page.ui.row(row, width=(80, "%"), align="center")
+
+quote = page.ui.banners.quote(fintech_analytics.QUOTE, size_notch=15, author="User feedbacks", background="#464646")
+quote.style.css.color = "#bbb"
+quote.style.css.padding = "40px 10%"
+quote.style.css.margin = "40px 0"
+
+about_epyk = page.ui.banners.title(fintech_analytics.BANNER_ABOUT['title'], fintech_analytics.BANNER_ABOUT['content'])
+about_epyk.style.css.padding = "40px 10%"
+
+footer = page.ui.banners.text("", background="black")
